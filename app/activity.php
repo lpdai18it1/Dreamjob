3a616c7c979815419eef4dadfc3eadd820c999eb
lpdai18it1 <44639422+lpdai18it1@users.noreply.github.com>
2020-01-04 03:06:55 +0700

    dai

--- app/activity.php
+++ app/activity.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class activity extends Model
 {
-    //
+    protected $table ="activity";
+   	public function children()
+    {
+    	return $this->hasMany('app\children','id_children','id');
+    }
 }
--- app/chart.php
+++ app/chart.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class chart extends Model
 {
-    //
+	protected $table = "chart";
+   	public function children()
+    {
+    	return $this->hasMany('app\children','id_children','id');
+    }
 }
